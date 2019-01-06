0f8b9902a2077e2db92512eedc8821cbeaa9aace
SeanBrennan645git <16020316@students.southwales.ac.uk>
2019-01-06 13:44:22 +0000

    Initial commit

---
+++ Assets/MyScripts/SpeedScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedScript : MonoBehaviour
+{
+    
+    public float speed;
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (speed<10)
+        {
+            speed += 0.5f;
+        }
+	}
+}
